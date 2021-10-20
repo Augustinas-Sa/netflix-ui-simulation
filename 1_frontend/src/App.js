7cc76ce4ac42a9876687e040c9eb172f53c4025c
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-10-20 12:43:58 +0300

    Created navbar and style and navigation routes

--- 1_frontend/src/App.js
+++ 1_frontend/src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import HomePage from './pages/HomePage';
+import AboutUsPage from './pages/AboutUsPage';
+import ServicesPage from './pages/ServicesPage';
+import BlogPage from './pages/BlogPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
-  return <div>Netflix simulation</div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/about' component={AboutUsPage} />
+          <Route path='/services' component={ServicesPage} />
+          <Route path='/blog' component={BlogPage} />
+          <Route path='/contact' component={ContactPage} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
